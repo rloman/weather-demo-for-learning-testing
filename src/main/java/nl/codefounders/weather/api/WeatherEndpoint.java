bb9d7ca6f296306c850c980ebad697186dce1e78
Raymond Loman <raymond@carpago.nl>
2022-04-22 11:49:37 +0200

    Add it for endpoint finished

--- src/main/java/nl/codefounders/weather/api/WeatherEndpoint.java
+++ src/main/java/nl/codefounders/weather/api/WeatherEndpoint.java
@@ -1,12 +1,17 @@
 package nl.codefounders.weather.api;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import nl.codefounders.weather.model.Weather;
 import nl.codefounders.weather.service.WeatherService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api/weather")
@@ -18,12 +23,6 @@ public class WeatherEndpoint {
     @PostMapping
     public Weather create(@RequestBody Weather source) {
         return this.weatherService.save(source);
-    }
-
-
-    @GetMapping
-    public List<Weather> findAll() {
-        return this.weatherService.findAll();
     }
 
     @GetMapping("{city}")
--- src/test/java/nl/codefounders/weather/integration/WeatherEndpointIT.java
+++ src/test/java/nl/codefounders/weather/integration/WeatherEndpointIT.java
@@ -1,8 +1,13 @@
 package nl.codefounders.weather.integration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
+import javax.annotation.PostConstruct;
+
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -13,37 +18,68 @@ import org.springframework.http.ResponseEntity;
 import nl.codefounders.weather.model.Weather;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.MethodName.class)
 public class WeatherEndpointIT {
-	
-	@Autowired  // (see this as Postman)
+
+	@Autowired // (see this as Postman)
 	private TestRestTemplate restTemplate;
-	
-	private String host = "localhost";
-	
+
 	@LocalServerPort
 	private int localPort;
-	
-	private String api = "api/weather";
-	
+
+	private String baseApi;
+
+	@PostConstruct
+	public void postConstruct() {
+		String host = "localhost";
+		String api = "api/weather";
+
+		baseApi = String.format("http://%s:%d/%s", host, localPort, api);
+	}
 
 	@Test
 	public void test1CreateUsingPost() {
-		
+
 		// Given
 		Weather almelo = new Weather();
 		almelo.setCity("Almelo");
 		almelo.setTemperature(12.0);
-		
-		String url = "http://"+host+":"+localPort+"/"+api;
-		System.err.println("url is: "+url);
-		
+
+
 		// When
-		ResponseEntity<Weather> response = restTemplate.postForEntity(url, almelo, Weather.class);
-		
+		ResponseEntity<Weather> response = restTemplate.postForEntity(baseApi, almelo, Weather.class);
+
 		// Then
 		Weather responseWeather = response.getBody();
-		
+
 		assertEquals("Almelo", responseWeather.getCity());
 		assertEquals(12.0, responseWeather.getTemperature());
+	}
+
+	@Test
+	public void test2FindByCity() {
+
+		// Given
+
+		// When
+		ResponseEntity<Weather> response = restTemplate.getForEntity(baseApi + "/" + "Almelo", Weather.class);
+
+		// Then
+		Weather result = response.getBody();
+
+		assertEquals("Almelo", result.getCity());
+		assertEquals(12.0, result.getTemperature());
+
+	}
+	
+	@Test
+	public void test3FindNotExistingCity() {
+		ResponseEntity<Weather> response = restTemplate.getForEntity(baseApi + "/" + "Utrecht", Weather.class);
+
+		// Then
+		Weather result = response.getBody();
+		
+		assertNull(result);
+	
 	}
 }
