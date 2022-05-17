cc831849a7c44926a34d16305401c7c63d91f31b
Raymond Loman <raymond@carpago.nl>
2022-05-17 21:30:55 +0200

    Add showcase for Spring Boot Actuator and some extras.
    commit 4c20223a7419b376ca1fe97d2d88a36d4ebb510d
    Author: Raymond Loman <raymond@carpago.nl>
    Date:   Tue May 17 21:26:06 2022 +0200
    
        Add demo using Spring Boot Actuator with jq and grep -E to filter out the beans we made in this app
    
    commit 7ada32bcc27f6aa04ba78f7cb7964ecc38c458b1
    Author: Raymond Loman <raymond@carpago.nl>
    Date:   Tue May 17 21:00:28 2022 +0200
    
        Add showcase for Spring Boot Actuator
    
        	Add spring-boot-starter-actuator dependency.
        	Add some nice properties in application.properties.
        	Add interesting.endpoints.for.actuator.txt for some handy actuator enpoints.

--- src/main/java/nl/codefounders/weather/api/WeatherEndpoint.java
+++ src/main/java/nl/codefounders/weather/api/WeatherEndpoint.java
@@ -1,20 +1,13 @@
 package nl.codefounders.weather.api;
 
-import java.util.Optional;
-
 import nl.codefounders.weather.WeatherConfig;
-import org.apache.tomcat.util.descriptor.web.SecurityRoleRef;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import nl.codefounders.weather.model.Weather;
 import nl.codefounders.weather.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/weather")
